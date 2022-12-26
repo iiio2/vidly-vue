ffb3db5da50ee24bd5efa8214bcf138b8ab216f4
Mr. Robot <gomexxxx71@gmail.com>
2022-12-26 11:38:24 +0600

    initial commit.

---
+++ src/components/movies.vue
@@ -0,0 +1,32 @@
+<template>
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Genre</th>
+        <th>Number In Stock</th>
+        <th>Daily Rental Rate</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="movie in movies" :key="movie._id">
+        <td>{{ movie.title }}</td>
+        <td>{{ movie.genre.name }}</td>
+        <td>{{ movie.numberInStock }}</td>
+        <td>{{ movie.dailyRentalRate }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { onMounted } from "vue";
+import { getMovies } from "../services/fakeMovieService";
+
+const movies = ref(getMovies());
+
+onMounted(() => {
+  movies.value;
+});
+</script>
